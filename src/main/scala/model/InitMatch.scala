98b3093e0c35116db2b275b1ca6e20cfde1f34e8
gbgDalmont <lorenzo.dalmonte4@studio.unibo.it>
2026-06-26 13:12:40 +0200

    Added and tested MatchBuilder

--- src/main/scala/model/InitMatch.scala
+++ src/main/scala/model/InitMatch.scala
@@ -1,5 +1,7 @@
 package model
 
+import scala.util.Random
+
 enum Color:
   case Orange, Blue, Green, Black
   
@@ -36,3 +38,65 @@ class PlayerFactoryImpl extends PlayerFactory:
     else
       playerList = playerList.concat(List(newPlayer))
       Some[Player](newPlayer)
+
+/**
+ * A match that keeps track of rounds left and current player in action
+ */
+trait Match:
+  /**
+   * @return how many rounds are left before the end of the game
+   */
+  def remainingRounds: Int
+
+  /**
+   * A method that passes the turn to the next player
+   */
+  def passTurn(): Unit
+
+  /**
+   * @return the player currently in control
+   */
+  def getCurrentPlayer: Player
+
+  /**
+   * @return true if the game is over, false otherwise
+   */
+  def gameOver: Boolean
+
+/**
+ * A builder that creates a match after receiving enough players to start the game
+ */
+trait MatchBuilder:
+  /**
+   * A method that adds a player to the match
+   *
+   * @param player the player to be added to the game
+   * @return a MatchBuilder with the newly added player
+   */
+  def addPlayer(player: Player): MatchBuilder
+
+  /**
+   * A method that tells you if there is enough info to start the game
+   *
+   * @return true if build can be called, false otherwise
+   */
+  def ready: Boolean
+
+  /**
+   * A method that takes the gathered info and builds the match accordingly
+   *
+   * @return a new Match involving the previously added players
+   */
+  def build: Match
+
+class MatchBuilderImpl(playerAmount: Int) extends MatchBuilder:
+  private var playerList: Seq[Player] = List.empty
+
+  def addPlayer(player: Player): MatchBuilder =
+    playerList = playerList.concat(List(player))
+    this
+
+  def ready: Boolean =
+    playerList.size >= playerAmount
+
+  def build: Match = ???
--- src/test/scala/model/InitMatch.scala
+++ src/test/scala/model/InitMatch.scala
@@ -19,3 +19,14 @@ class InitMatch extends AnyFlatSpec with Matchers:
     val player2 = playerFactory.create("Luigi", Color.Orange)
 
     player2 should be(Option.empty)
+
+  "A MatchBuilder" should "be ready only after adding every Player" in:
+    var matchBuilder: MatchBuilder = MatchBuilderImpl(2)
+    val playerFactory = PlayerFactoryImpl()
+    val player1 = playerFactory.create("Mario", Color.Orange)
+    matchBuilder = matchBuilder.addPlayer(player1.get)
+    matchBuilder.ready should be(false)
+  
+    val player2 = playerFactory.create("Luigi", Color.Green)
+    matchBuilder = matchBuilder.addPlayer(player2.get)
+    matchBuilder.ready should be(true)
