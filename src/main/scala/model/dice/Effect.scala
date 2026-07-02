39c12e379defc1ab914ce18e6cbf4cf9e7fced8d
gbgDalmont <lorenzo.dalmonte4@studio.unibo.it>
2026-07-02 06:58:23 +0200

    Added ResourceEffect implementations

--- src/main/scala/model/dice/Effect.scala
+++ src/main/scala/model/dice/Effect.scala
@@ -1,6 +1,31 @@
 package model.dice
 
-enum Effect:
-  case ResourceEffect(amount: Int)
-  case MultiplierEffect(multiplier: Int)
-  case CopyEffect()
+import model.resource.PlayerResources
+
+trait Effect:
+  def effectResources: PlayerResources
+  def applyResourceChange(playerResources: PlayerResources,
+    fun: (PlayerResources, PlayerResources) => PlayerResources = _ + _): PlayerResources
+
+case class ResourceEffect(effectResources: PlayerResources) extends Effect:
+  override def applyResourceChange(playerResources: PlayerResources,
+                                  fun: (PlayerResources, PlayerResources) => PlayerResources): PlayerResources =
+    fun(playerResources, effectResources)
+
+trait NonFixedEffect extends Effect:
+  def setCurrentEffect(effect: ResourceEffect): Unit
+
+case class MultiplierEffect(multiplier: Int) extends NonFixedEffect:
+  private var currentEffect: Effect = ResourceEffect(PlayerResources.setResources(0,0,0,0))
+
+  override def setCurrentEffect(effect: ResourceEffect): Unit =
+    currentEffect = ResourceEffect(effect.effectResources * multiplier)
+
+  override def effectResources: PlayerResources =
+    currentEffect.effectResources
+
+  override def applyResourceChange(playerResources: PlayerResources,
+                                   fun: (PlayerResources, PlayerResources) => PlayerResources): PlayerResources =
+    currentEffect.applyResourceChange(playerResources)
+
+class CopyEffect extends MultiplierEffect(1)
--- src/main/scala/model/resource/Resource.scala
+++ src/main/scala/model/resource/Resource.scala
@@ -14,7 +14,6 @@ trait Resource[A <: ResourceType]:
 case class ResourceImpl[A <: ResourceType](currentAmount: Int, maxCapacity: Option[Int]) extends Resource[A]
 
 object Resource:
-
   private def resourceCheck[A <: ResourceType](res: Resource[A]): Resource[A] =
     var amount = res.currentAmount
     if res.maxCapacity.isDefined
@@ -38,10 +37,14 @@ object Resource:
       else first
 
 object ResourceFactory:
-  def gold(amount: Int): Resource[Gold] = ResourceImpl[Gold](amount, Some(12))
-  def sunCrystal(amount: Int): Resource[SunCrystal] = ResourceImpl[SunCrystal](amount, Some(6))
-  def moonCrystal(amount: Int): Resource[MoonCrystal] = ResourceImpl[MoonCrystal](amount, Some(6))
-  def victoryPoint(amount: Int): Resource[VictoryPoint] = ResourceImpl[VictoryPoint](amount, Option.empty)
+  def gold(amount: Int, cap: Option[Int] = Some(12)): Resource[Gold] =
+    ResourceImpl[Gold](amount, cap)
+  def sunCrystal(amount: Int, cap: Option[Int] = Some(6)): Resource[SunCrystal] =
+    ResourceImpl[SunCrystal](amount, cap)
+  def moonCrystal(amount: Int, cap: Option[Int] = Some(6)): Resource[MoonCrystal] =
+    ResourceImpl[MoonCrystal](amount, cap)
+  def victoryPoint(amount: Int, cap: Option[Int] = Option.empty): Resource[VictoryPoint] =
+    ResourceImpl[VictoryPoint](amount, cap)
 
 case class PlayerResources(gold: Resource[Gold],
                            sunCrystals: Resource[SunCrystal],
@@ -49,14 +52,39 @@ case class PlayerResources(gold: Resource[Gold],
                            victoryPoints: Resource[VictoryPoint])
 
 object PlayerResources:
-  def setPlayerResources(gold: Int,
-                         sunCrystals: Int,
-                         moonCrystals: Int,
-                         victoryPoints: Int): PlayerResources =
+  extension [A <: ResourceType](first: PlayerResources)
+    private def applyFun(other: PlayerResources, fun: (Int, Int) => Int): PlayerResources =
+      first match
+        case PlayerResources(g, s, m, v) => PlayerResources(
+          ResourceFactory.gold(fun(g.currentAmount, other.gold.currentAmount), g.maxCapacity),
+          ResourceFactory.sunCrystal(fun(s.currentAmount, other.sunCrystals.currentAmount), s.maxCapacity),
+          ResourceFactory.moonCrystal(fun(m.currentAmount, other.moonCrystals.currentAmount), m.maxCapacity),
+          ResourceFactory.victoryPoint(fun(v.currentAmount, other.victoryPoints.currentAmount), v.maxCapacity)
+        )
+
+    def +(other: PlayerResources): PlayerResources =
+      first.applyFun(other, _ + _)
+
+    def -(other: PlayerResources): PlayerResources =
+      first.applyFun(other, _ - _)
+
+    def *(multiplier: Int): PlayerResources =
+      first match
+        case PlayerResources(g, s, m, v) => PlayerResources(
+          g * multiplier,
+          s * multiplier,
+          m * multiplier,
+          v * multiplier
+        )
+  
+  def setResources(gold: Int,
+                   sunCrystals: Int,
+                   moonCrystals: Int,
+                   victoryPoints: Int): PlayerResources =
     PlayerResources(ResourceFactory.gold(gold),
       ResourceFactory.sunCrystal(sunCrystals),
       ResourceFactory.moonCrystal(moonCrystals),
       ResourceFactory.victoryPoint(victoryPoints))
 
   def emptyPlayerResources: PlayerResources =
-    setPlayerResources(0,0,0,0)
+    setResources(0,0,0,0)
