109feaefc31ad329bb4b635dc6b2fcc94d8ffc67
Galileo Foschini <galileo.foschini@studio.unibo.it>
2026-07-09 11:16:18 +0200

    refactoring ResourceConverters

--- src/main/scala/controller/converters/ResourceConverters.scala
+++ src/main/scala/controller/converters/ResourceConverters.scala
@@ -3,16 +3,22 @@ package controller.converters
 import model.resource.*
 
 object ResourceConverters:
-  
+  val GoldString = "Oro"
+  val SunCrystalString = "Cristalli Solari"
+  val MoonCrystalString = "Cristalli Lunari"
+  val GloryPointString = "Punti Gloria"
+
   def resourceToString(resource: Resource): String = resource match
-    case Gold(_) => "Oro"
-    case SunCrystal(_) => "Cristalli Sola"
-    case MoonCrystal(_) => "Cristalli Lunari"
-    case GloryPoint(_) => "Punti Gloria"
-    
+    case Gold(_) => GoldString
+    case SunCrystal(_) => SunCrystalString
+    case MoonCrystal(_) => MoonCrystalString
+    case GloryPoint(_) => GloryPointString
+    case _ => throw IllegalArgumentException(s"$resource is not a recognized resource")
+
   def stringToResourceBuilder(string: String)(amount: Int):Resource =string match
-    case "Oro" => Gold(amount)
-    case "Cristalli Solari" => SunCrystal(amount)
-    case "Cristalli Lunari" => MoonCrystal(amount)
-    case "Punti Gloria" => GloryPoint(amount)
+    case GoldString => Gold(amount)
+    case SunCrystalString => SunCrystal(amount)
+    case MoonCrystalString => MoonCrystal(amount)
+    case GloryPointString => GloryPoint(amount)
+    case _ => throw IllegalArgumentException(s"`$string` doesn't correspond to any recognized resource")
 
