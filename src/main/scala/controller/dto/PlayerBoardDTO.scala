2e5290cdb585c46d69cc387430c1ff4f03d20da3
Galileo Foschini <galileo.foschini@studio.unibo.it>
2026-07-10 10:19:48 +0200

    Added resourceMaxMap to PlayerBoardDTO

--- src/main/scala/controller/dto/PlayerBoardDTO.scala
+++ src/main/scala/controller/dto/PlayerBoardDTO.scala
@@ -3,14 +3,23 @@ package controller.dto
 import model.resource.*
 import controller.converters.ResourceConverters.*
 
-case class PlayerBoardDTO(resourceMap: Map[String, Int])
+case class PlayerBoardDTO(resourceMap: Map[String, Int], resourceMaxMap: Map[String, Int])
 
 object PlayerBoardDTO:
   def apply(board: PlayerBoard): PlayerBoardDTO =
-    PlayerBoardDTO(Map(
-      resourceToString(board.gold.resource) -> board.gold.amount,
-      resourceToString(board.sunCrystals.resource) -> board.sunCrystals.amount,
-      resourceToString(board.moonCrystals.resource) -> board.moonCrystals.amount,
-      resourceToString(board.gloryPoints) -> board.gloryPoints.amount
-    ))
+    def extractResourcesAmounts(playerBoard: PlayerBoard): Map[String, Int] = Map(
+        resourceToString(board.gold.resource) -> board.gold.amount,
+        resourceToString(board.sunCrystals.resource) -> board.sunCrystals.amount,
+        resourceToString(board.moonCrystals.resource) -> board.moonCrystals.amount,
+        resourceToString(board.gloryPoints) -> board.gloryPoints.amount
+      )
+    def extractResourceMaximumAmounts(playerBoard: PlayerBoard): Map[String, Int] = Map(
+        resourceToString(board.gold.resource) -> board.gold.maxCapacity,
+        resourceToString(board.sunCrystals.resource) -> board.sunCrystals.maxCapacity,
+        resourceToString(board.moonCrystals.resource) -> board.moonCrystals.maxCapacity
+      )
+    PlayerBoardDTO(
+      extractResourcesAmounts(board),
+      extractResourceMaximumAmounts(board)
+    )
 
