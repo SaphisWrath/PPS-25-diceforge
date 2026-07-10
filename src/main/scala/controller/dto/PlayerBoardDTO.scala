52ee93601908017fa29effbbc68edcb762a5f21e
Galileo Foschini <galileo.foschini@studio.unibo.it>
2026-07-10 10:11:45 +0200

    Fixed error in PlayerBoardDTO

--- src/main/scala/controller/dto/PlayerBoardDTO.scala
+++ src/main/scala/controller/dto/PlayerBoardDTO.scala
@@ -8,9 +8,9 @@ case class PlayerBoardDTO(resourceMap: Map[String, Int])
 object PlayerBoardDTO:
   def apply(board: PlayerBoard): PlayerBoardDTO =
     PlayerBoardDTO(Map(
-      resourceToString(board.gold) -> board.gold.amount,
-      resourceToString(board.sunCrystals) -> board.sunCrystals.amount,
-      resourceToString(board.moonCrystals) -> board.moonCrystals.amount,
+      resourceToString(board.gold.resource) -> board.gold.amount,
+      resourceToString(board.sunCrystals.resource) -> board.sunCrystals.amount,
+      resourceToString(board.moonCrystals.resource) -> board.moonCrystals.amount,
       resourceToString(board.gloryPoints) -> board.gloryPoints.amount
     ))
 
