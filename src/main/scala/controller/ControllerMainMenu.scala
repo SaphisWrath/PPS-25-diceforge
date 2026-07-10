b938594a2a7619019a5c2c8153e1a336a718270d
Giulia <giulia.bonifazi@studio.unibo.it>
2026-07-10 17:58:16 +0200

    changed MainMenu Controller into singleton Controller

--- src/main/scala/controller/ControllerMainMenu.scala
+++ src/main/scala/controller/ControllerMainMenu.scala
@@ -6,19 +6,6 @@ import javafx.event.ActionEvent
 import scalafx.scene.Scene
 import view.scenes.MainMenuScene
 
-trait ControllerMainMenu:
-  def scene: Scene
-  def onStart: ActionEvent => Unit
-  def onRules: ActionEvent => Unit
-
 object ControllerMainMenu:
-  private class ControllerMainMenuImpl(
-                                        val onStart: ActionEvent => Unit,
-                                        val onRules: ActionEvent => Unit)
-  extends ControllerMainMenu:
-    val scene = new MainMenuScene(this)
+  def scene(onStart: ActionEvent => Unit, onRules: ActionEvent => Unit) = MainMenuScene(onStart, onRules)
   
-  def apply(
-             onStart: ActionEvent => Unit,
-             onRules: ActionEvent => Unit
-           ): ControllerMainMenu = new ControllerMainMenuImpl(onStart, onRules)
