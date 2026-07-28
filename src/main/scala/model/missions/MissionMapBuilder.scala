4de930f46b285ce2e003b5b55b00d0ba316bd36c
Giulia <giulia.bonifazi@studio.unibo.it>
2026-07-28 14:51:15 +0200

    Cleaned up population of mission Map inside GameMatch. Currently placeholder form. Adding missing file

---
+++ src/main/scala/model/missions/MissionMapBuilder.scala
@@ -0,0 +1,19 @@
+package model.missions
+
+import model.effects.ResourceEffect
+import model.resource.{Gold, MoonCrystal, SunCrystal}
+
+object MissionMapBuilder:
+  def makePlaceholderMissions: Map[Int, List[Mission]] =
+    val cost: List[ResourceEffect] = (ResourceEffect(SunCrystal(3)), ResourceEffect(MoonCrystal(3))).toList
+    val reward: List[ResourceEffect] = List(ResourceEffect(Gold(3)))
+    val placeholderMission = InstantMission(reward, cost, "one")
+    List(
+      0 -> List(placeholderMission, placeholderMission),
+      1 -> List(placeholderMission, placeholderMission),
+      2 -> List(placeholderMission, placeholderMission),
+      3 -> List(placeholderMission, placeholderMission),
+      4 -> List(placeholderMission, placeholderMission),
+      5 -> List(placeholderMission, placeholderMission),
+      6 -> List(placeholderMission, placeholderMission, placeholderMission)
+    ).toMap
