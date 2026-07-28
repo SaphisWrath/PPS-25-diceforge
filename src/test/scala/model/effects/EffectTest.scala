b8fc07a5aff9bbf24a3fec96bbb03e790182647e
Giulia <giulia.bonifazi@studio.unibo.it>
2026-07-28 14:09:34 +0200

    Changed EffectTest to be easier to test specific cases with

--- src/test/scala/model/effects/EffectTest.scala
+++ src/test/scala/model/effects/EffectTest.scala
@@ -8,18 +8,15 @@ import org.scalatest.flatspec.AnyFlatSpec
 class EffectTest extends AnyFlatSpec:
   "A ResourceEffect" should "increase or decrease the player's resources by the given amount" in:
     val playerBoard = PlayerBoard.emptyBoard(Player("Mario", Orange))
-    val amount = 10
+    val amount = 20
     val resourceEffect = ResourceEffect(Gold(amount), Option(playerBoard))
     resourceEffect.resolve()
-    assert(playerBoard.gold.amount == amount)
+    val expectedAdd1 = if playerBoard.gold.maxCapacity > amount then amount else playerBoard.gold.maxCapacity
+    assert(playerBoard.gold.amount == expectedAdd1)
+    val expectedAdd2 = if playerBoard.gold.maxCapacity > amount * 2 then amount * 2 else playerBoard.gold.maxCapacity
     resourceEffect.resolve()
-    assert(playerBoard.gold.amount == {
-      if playerBoard.gold.maxCapacity > amount * 2 then amount * 2 else playerBoard.gold.maxCapacity
-    })
-
+    assert(playerBoard.gold.amount == expectedAdd2)
     resourceEffect.setModule(model.utils.ResourceEffectModules.SubtractResource)
-    val expected = playerBoard.gold.amount - resourceEffect.resource.amount
+    val expectedSub = expectedAdd2 - resourceEffect.resource.amount
     resourceEffect.resolve()
-    assert(playerBoard.gold.amount == {
-      if expected >= 0 then expected else 0
-    })
+    assert(playerBoard.gold.amount == (if expectedSub >= 0 then expectedSub else 0))
