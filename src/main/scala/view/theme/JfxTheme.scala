7968fb9e3ac73f2d0502c337fff2f68645369e8d
Giulia <giulia.bonifazi@studio.unibo.it>
2026-08-02 15:49:15 +0200

    Added error colour to theme

--- src/main/scala/view/theme/JfxTheme.scala
+++ src/main/scala/view/theme/JfxTheme.scala
@@ -7,32 +7,32 @@ object JfxTheme extends Theme[Color]:
 
   override def primary: Color = Color.valueOf("#FFC136")
 
-  override def secondary: Color = Color.rgb(96, 96, 96)
+  override def secondary: Color = Color.valueOf("#606060")
 
   override def tertiary: Color = Color.valueOf("#367BFF")
 
   override def primaryContainer: Color = Color.valueOf("#FFCB5C")
 
-  override def secondaryContainer: Color = Color.rgb(128, 128, 128)
+  override def secondaryContainer: Color = Color.valueOf("#808080")
 
   override def tertiaryContainer: Color = Color.valueOf("#5C95FF")
 
   override def onPrimaryContainer: Color = Color.valueOf("#1A1100")
 
-  override def onSecondaryContainer: Color = Color.rgb(64, 64, 64)
+  override def onSecondaryContainer: Color = Color.valueOf("#404040")
 
   override def onTertiaryContainer: Color = Color.valueOf("#001947")
 
   override def primaryBorder: Color = Color.valueOf("#FF5B36")
 
-  override def secondaryBorder: Color = ???
+  override def secondaryBorder: Color = Color.valueOf("#242424")
 
   override def tertiaryBorder: Color = Color.valueOf("#0049D1")
 
-  override def error: Color = ???
+  override def error: Color = Color.valueOf("#FF542E")
 
-  override def errorContainer: Color = ???
+  override def errorContainer: Color = Color.valueOf("#FF7A5C")
 
-  override def onErrorContainer: Color = ???
+  override def onErrorContainer: Color = Color.valueOf("#470D00")
 
-  override def errorBorder: Color = ???
+  override def errorBorder: Color = Color.valueOf("#A31E00")
