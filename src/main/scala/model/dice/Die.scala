07dd643dd28ee3b69742d27fba4c7f7bf4c8b2bc
Giulia <giulia.bonifazi@studio.unibo.it>
2026-08-04 21:55:50 +0200

    Die now allows to swap faces

--- src/main/scala/model/dice/Die.scala
+++ src/main/scala/model/dice/Die.scala
@@ -6,7 +6,7 @@ import model.utils.RandomModule
 trait Die:
   def roll(using randomModule: RandomModule[Int]): Effect
 
-  def addFaces(addedFaces: Effect*): Unit
+  def addFace(newFace: Effect, replacedFace: Option[Effect] = None): Unit
 
   def faces: Seq[Effect]
 
@@ -14,17 +14,22 @@ object Die:
   private class BaseDie(numFaces: Int) extends Die:
     private var _faces: Seq[Effect] = Seq.empty
     private val maxFaces: Int = numFaces
-    private val isFull: Boolean = numFaces == _faces.length
 
-    private def addFace(face: Effect): Unit =
-      if !isFull then _faces = _faces.+:(face)
-
-    override def addFaces(addedFaces: Effect*): Unit =
-      addedFaces.foreach(addFace)
+    private def isFull: Boolean = numFaces == _faces.length
 
     override def roll(using randomModule: RandomModule[Int]): Effect =
       _faces(randomModule.randomIndex(maxFaces))
 
     override def faces: Seq[Effect] = _faces
+
+    override def addFace(newFace: Effect, replacedFace: Option[Effect]): Unit =
+      if !isFull then
+        _faces = _faces.+:(newFace)
+      else
+        replacedFace match
+          case Some(oldFace) =>
+            _faces = _faces.diff(Seq(oldFace))
+            addFace(newFace)
+          case _ => throw IllegalStateException("Max number of faces was reached but no replacedFace was provided.")
 
   def apply(numFaces: Int): Die = BaseDie(numFaces)
--- src/test/scala/model/dice/DieTest.scala
+++ src/test/scala/model/dice/DieTest.scala
@@ -1,27 +1,44 @@
 package model.dice
 
 import model.dice.*
-import model.dice.Face.*
 import model.effects.Target.Self
-import model.effects.{Effect, ResourceEffect}
+import model.effects.{CopyEffect, Effect, MultiplyEffect, ResourceEffect}
 import model.resource.Gold
+import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatestplus.mockito.MockitoSugar.mock
 
-class DieTest extends AnyFunSuite {
+class DieSuite extends AnyFunSuite {
   val face: Effect = ResourceEffect(Gold(10), Self)
 
-  test("Die requires correct number of faces.") {
+  test("addFace") {
     val maxFaces = 10
     val die = Die(maxFaces)
-    for (i <- Iterable.range(0, maxFaces)) die.addFaces(face)
+    for (i <- Iterable.range(0, maxFaces)) die.addFace(face)
     assert(die.faces.length == maxFaces)
   }
 
-  test("Die roll returns an effect."){
+  test("roll"){
     import model.utils.RandomModules.given
     val die = Die(1)
-    die.addFaces(face)
+    die.addFace(face)
     assert(die.roll.isInstanceOf[Effect])
   }
 }
+
+class DieSpec extends AnyFlatSpec:
+  val maxFaces = 6
+  val phEffect = ResourceEffect(Gold(3), Self)
+
+  "Any die" should "allow to swap a selected face with a new face only when it's already full" in:
+    val die = Die(maxFaces)
+    val oldFace = CopyEffect()
+    for (i <- Iterable.range(0, maxFaces - 1)) die.addFace(phEffect)
+    die.addFace(oldFace)
+    assert(die.faces.contains(oldFace))
+    val newFace = MultiplyEffect(3)
+    die.addFace(newFace, Some(oldFace))
+    assert(die.faces.contains(newFace))
+    assert(!die.faces.contains(oldFace))
+    assertThrows[IllegalStateException](die.addFace(phEffect))
+
