e63bdc13577eccdfa14019e85e44ff71f22f22af
gbgDalmont <lorenzo.dalmonte4@studio.unibo.it>
2026-08-06 15:07:29 +0200

    Edited DieDTO

--- src/main/scala/controller/dto/DieDTO.scala
+++ src/main/scala/controller/dto/DieDTO.scala
@@ -4,8 +4,7 @@ import model.dice.Die
 
 trait DieDTO:
   def faces: Seq[EffectDTO]
-  def index: Int
 
 object DieDTO:
-  private case class DieDTOImpl(faces: Seq[EffectDTO], index: Int) extends DieDTO
-  def apply(die: Die, index: Int): DieDTO = DieDTOImpl(die.faces.map(EffectDTO(_)), index)
+  private case class DieDTOImpl(faces: Seq[EffectDTO]) extends DieDTO
+  def apply(die: Die): DieDTO = DieDTOImpl(die.faces.map(EffectDTO(_)))
