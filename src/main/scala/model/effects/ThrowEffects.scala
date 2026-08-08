60c3ae8112d3ec15f34e4d75cd9e52b38379ec52
Giulia <giulia.bonifazi@studio.unibo.it>
2026-08-08 18:59:29 +0200

    Implemented ThrowTimes

--- src/main/scala/model/effects/ThrowEffects.scala
+++ src/main/scala/model/effects/ThrowEffects.scala
@@ -1,6 +1,7 @@
 package model.effects
 
 import model.Players.Player
+import model.effects
 import model.utils.RandomModules.given_RandomModule_Int
 import model.utils.ResourceEffectModules.SubtractResource
 
@@ -10,30 +11,36 @@ trait ThrowAction:
   def throwDice(receivers: Seq[Player]): Unit
 
 trait SubtractThrow extends ThrowAction:
-  abstract override def throwDice(receiver: Player): Unit = {
+  abstract override def throwDice(receiver: Player): Unit =
     super.throwDice(receiver)
     results.foreach((p, e) => e match
       case r: ResourceEffect => r.setModule(SubtractResource)
     )
-  }
 
 object ThrowEffects:
-  class ThrowAllDice extends Effect with ThrowAction:
+  class ThrowAllDice(times: Int = 1) extends Effect with ThrowAction:
     protected var results: Seq[(Player, Effect)] = Seq.empty
 
     override def throwDice(receiver: Player): Unit =
       results = results.concat(receiver.dice.map(d => (receiver, d.roll)))
-    
+
 
     override def resolve(receiver: Player): Unit =
-      throwDice(receiver)
-      EffectManager().attemptSolve(results)
+      Array.range(0, times).foreach(_ =>
+        throwDice(receiver)
+        EffectManager().attemptSolve(results)
+        results = Seq.empty
+      )
 
     override def resolve(receivers: Seq[Player]): Unit =
-      throwDice(receivers)
-    
+      Array.range(0, times).foreach(_ =>
+        throwDice(receivers)
+        EffectManager().attemptSolve(results)
+        results = Seq.empty
+      )
+
     override def throwDice(receivers: Seq[Player]): Unit =
       receivers.foreach(throwDice)
-      EffectManager().attemptSolve(results)
 
   class ThrowSubtractEffect extends ThrowAllDice with SubtractThrow
+  class ThrowTimesEffect(times: Int = 1) extends ThrowAllDice(times)
--- src/test/scala/model/effects/ThrowEffectTest.scala
+++ src/test/scala/model/effects/ThrowEffectTest.scala
@@ -4,8 +4,8 @@ import mock.MockPlayer
 import model.Players.Color.{Black, Blue, Orange}
 import model.dice.Die
 import model.effects.Target.Self
-import model.effects.ThrowEffects.ThrowSubtractEffect
-import model.resource.Gold
+import model.effects.ThrowEffects.{ThrowSubtractEffect, ThrowTimesEffect}
+import model.resource.{Gold, PlayerBoard}
 import org.scalatest.flatspec.AnyFlatSpec
 
 class ThrowEffectTest extends AnyFlatSpec:
@@ -19,9 +19,25 @@ class ThrowEffectTest extends AnyFlatSpec:
     players.zipWithIndex.foreach((p, i) =>
       p.dice = Seq(mockDie, mockDie)
       p.board.addResource(res.copy(res.amount * 5))
-      expected(i) = p.board.gold.amount - (res.amount * 2)
+      expected(i) = math.max(p.board.gold.amount - (res.amount * 2), 0)
     )
     ThrowSubtractEffect().resolve(players)
     players.zipWithIndex.foreach((p, i) =>
       assert(p.board.gold.amount == expected(i))
-  )
+    )
+
+  "A ThrowTimesEffect" should "throw the dice the specified amount of times" in:
+    val players: Seq[MockPlayer] = newPlayers
+    val res = Gold(1)
+    val throws = 2
+    val mockDie = Die(Seq(ResourceEffect(res, Self)))
+    val expected: Array[Int] = Array.ofDim(players.length)
+    players.zipWithIndex.foreach((p, i) =>
+      p.dice = Seq(mockDie, mockDie)
+      p.board = PlayerBoard.emptyBoard
+      expected(i) =  math.min(p.board.gold.amount + (res.amount * 2 * throws), p.board.gold.maxCapacity)
+    )
+    ThrowTimesEffect(throws).resolve(players)
+    players.zipWithIndex.foreach((p, i) =>
+      assert(p.board.gold.amount == expected(i))
+    )
