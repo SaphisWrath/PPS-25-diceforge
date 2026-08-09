d8b62b02ae258f8c3299295d53bb81dfe3283295
gbgDalmont <lorenzo.dalmonte4@studio.unibo.it>
2026-08-09 19:51:44 +0200

    Updated MissionDescriptions

--- src/main/scala/view/MissionDescriptions.scala
+++ src/main/scala/view/MissionDescriptions.scala
@@ -12,7 +12,7 @@ object MissionDescriptions:
     ("helmet", ("Elmo dell'invisibilità", "Ottieni 4 Punti Vittoria e ottieni una faccia \"Risultato per 3\" da applicare a uno dei tuoi dadi")),
     ("spirits", ("Spiriti Selvaggi", "Ottieni 2 Punti Vittoria, 3 Oro, e 3 Cristalli Lunari")),
     ("minotaur", ("Minotauro", "Ottieni 8 Punti Vittoria; tutti gli altri giocatori lanciano i loro dadi, ma perdono le risorse determinate dal lancio dei dadi invece di guadagnarle")),
-//    ("scorpion", ("", "")),
+    ("scorpion", ("Scorpione", "Ottieni 8 Punti Vittoria; tira entrambi i tuoi dadi due volte e ottieni i risultati")),
     ("mirror", ("Specchio dell'abisso", "Ottieni 10 Punti Vittoria e ottieni una faccia \"Copia\" da applicare a uno dei tuoi dadi")),
     ("sphinx", ("Sfinge", "Ottieni 10 Punti Vittoria, poi tiri uno dei tuoi dadi 4 volte e ne ottieni i risultati")),
     ("smith_hammer", ("Martello del fabbro", "Spendi 12 Oro per ottenere 20 Punti Vittoria")),
