eb72f254eaf27b481cb61b6874c82aa7e2af1fbb
Galileo Foschini <galileo.foschini@studio.unibo.it>
2026-08-11 13:20:11 +0200

    Added scalaDoc to Redrawable

--- src/main/scala/view/Redrawable.scala
+++ src/main/scala/view/Redrawable.scala
@@ -3,12 +3,25 @@ package view
 import scalafx.scene.Node
 import scalafx.scene.layout.StackPane
 
+/**Used to create a redrawable element in scalafx
+ *
+ */
 trait Redrawable:
+  /**The concrete component to use in the view
+   *
+   * @return A [[Node]] instance equivalent to the content of the Redrawable
+   */
   def component: Node
 
+  /**Notify the object to redraw itself
+   *
+   */
   def redraw(): Unit
 
-  def apply(): Node = component
+  /**An equivalent method to [[component]]
+   * @return the same result as [[component]]
+   */
+  private def apply(): Node = component
 
 object Redrawable:
   private class RedrawableImpl[E <: Node](private val componentProducer: () => E) extends Redrawable:
