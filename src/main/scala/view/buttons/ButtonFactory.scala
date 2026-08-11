b04d4530916e10a1b0611fc67f1ad66cd07a7b80
Giulia <giulia.bonifazi@studio.unibo.it>
2026-08-11 18:00:39 +0200

    Added ButtonFactory interface missing files

--- src/main/scala/view/buttons/ButtonFactory.scala
+++ src/main/scala/view/buttons/ButtonFactory.scala
@@ -1,60 +1,27 @@
 package view.buttons
 
-import controller.publishers.ViewPublisher.{ViewContext, ViewSubscriber}
-import controller.publishers.ViewPublisher.ViewContext.*
-import controller.publishers.ViewPublisher
-import javafx.event.{ActionEvent, EventHandler}
-import scalafx.scene.Node
-import scalafx.scene.control.Button
-import scalafx.scene.input.MouseEvent
-import scalafx.scene.layout.*
-import scalafx.scene.paint.Color
-import view.theme.JfxTheme
-import view.utils.ViewUtils
-
-object ButtonFactory:
+trait ButtonFactory[Button, Icon]:
+  /**
+   * @param text the button label
+   * @param onClick the button onClick
+   * @return A button styled for the main menu
+   */
   def makeMenuButton(
-                      text: String,
-                      onClick: ActionEvent => Unit
-                    ): Button =
-    new Button(text) {
-      minWidth = 100
-      minHeight = 50
-      onAction = event => onClick(event)
-      textFill = JfxTheme.onPrimaryContainer
-      background = ViewUtils.makeBackgroundFill(JfxTheme.primaryContainer, CornerRadii(10))
-      border = ViewUtils.makeBorder(JfxTheme.primaryBorder, CornerRadii(10))
-    }
+                     text: String,
+                     onClick: () => Unit
+                   ): Button
 
-  def makeBoardButton(buttonText: String, onClick: () => Unit, isDisabled: () => Boolean = () => false): Button =
-    val cornerRadii = CornerRadii(5)
-    class ButtonSubscriber extends Button with ViewSubscriber:
-      private def calculateColor(): Unit =
-        textFill = if disabled.value then JfxTheme.onSecondaryContainer else JfxTheme.onTertiaryContainer
-        background = ViewUtils.makeBackgroundFill(
-          if disabled.value then JfxTheme.secondaryContainer else JfxTheme.tertiaryContainer,
-          cornerRadii
-        )
-        border = ViewUtils.makeBorder(
-          if disabled.value then JfxTheme.secondaryBorder else JfxTheme.tertiaryBorder,
-          cornerRadii
-        )
+  /**
+   * @param buttonText the button lbel
+   * @param onClick the button onClick
+   * @param isDisabled a producer for the value of the disabled param of the button
+   * @return A button styled for the Board
+   */
+  def makeBoardButton(buttonText: String, onClick: () => Unit, isDisabled: () => Boolean = () => false): Button
 
-      text = buttonText
-      onAction = event => onClick()
-      disable = isDisabled()
-      calculateColor()
-      disable.onChange((_, _, newValue) => calculateColor())
-
-      override def update(context: ViewContext): Unit = context match
-        case _ => this.disable = isDisabled()
-
-    val button = ButtonSubscriber()
-    button.setPublisher(ViewPublisher())
-    button
-    
-  def makeChoiceButton(icon: Node, onClick: () => Unit): Button =
-    new Button {
-      graphic = icon
-      onAction = event => onClick()
-    }
+  /**
+   * @param icon the icon for the choice button
+   * @param onClick the onClick for the button
+   * @return a button styled for the choice dialog
+   */
+  def makeChoiceButton(icon: Icon, onClick: () => Unit): Button
