38b0fe1f340716320fdc12ba86fcbd610082f2c3
gbgDalmont <lorenzo.dalmonte4@studio.unibo.it>
2026-08-11 07:02:28 +0200

    Updated EffectManagerTest

--- src/test/scala/model/effects/EffectManagerTest.scala
+++ src/test/scala/model/effects/EffectManagerTest.scala
@@ -15,6 +15,7 @@ class EffectManagerTest extends AnyFlatSpec with Matchers:
   private var choiceCalls = 0
   private var successfulSolves = 0
   private val choiceListener = new ModelSubscriber {
+    this.setPublisher(ModelPublisher())
     override def update(context: ModelPublisher.ModelContext): Unit = context match
       case EffectChoiceContext => choiceCalls = choiceCalls + 1
       case ResourceContext => successfulSolves = successfulSolves + 1
@@ -25,13 +26,14 @@ class EffectManagerTest extends AnyFlatSpec with Matchers:
     choiceCalls = 0
     successfulSolves = 0
 
-  private val players = Seq(
+  private def newPlayers = Seq(
     MockPlayer("Mario", Orange),
     MockPlayer("Luigi", Green)
   )
 
   "An EffectManager" should "solve ResourceEffects without outer help" in:
     resetChoiceListenerRecord()
+    val players = newPlayers
     val goldAmount = 3
     val sunCrystalAmount = 2
     val effects = Seq(
@@ -46,6 +48,7 @@ class EffectManagerTest extends AnyFlatSpec with Matchers:
 
   "An EffectManager" should "handle SumEffects and MultiplyEffects accordingly" in:
     resetChoiceListenerRecord()
+    val players = newPlayers
     val gloryPointAmount = 3
     val moonCrystalAmount = 2
     val multiplier = 3
@@ -61,4 +64,33 @@ class EffectManagerTest extends AnyFlatSpec with Matchers:
     choiceCalls should be(0)
     players.head.board.gloryPoints.amount should be(gloryPointAmount * multiplier)
     players.head.board.moonCrystals.amount should be(moonCrystalAmount * multiplier)
+
+  "An EffectManager" should "ask the player how to solve OptionEffects and CopyEffects" in :
+    resetChoiceListenerRecord()
+    val players = newPlayers
+    val goldAmount = 3
+    val sunCrystalAmount = 2
+    val effects = Seq(
+      (players.head, OptionEffect(Seq(
+        ResourceEffect(Gold(goldAmount), Self),
+        ResourceEffect(SunCrystal(sunCrystalAmount), Self)
+      ))),
+      (players(1), CopyEffect())
+    )
+
+    effectManager.attemptSolve(effects, true)
+    effectManager.effectsToSolve.head should be((players(1), OptionEffect(Seq(effects.head._2))))
+    choiceCalls should be(1)
+
+    effectManager.attemptSolve(effectManager.effectsToSolve.map((p, e) => (p, e.effects.head)))
+    effectManager.effectsToSolve should contain(effects.head)
+    effectManager.effectsToSolve should contain((players(1), effects.head._2))
+    choiceCalls should be(2)
+
+    effectManager.attemptSolve(effectManager.effectsToSolve.map((p, e) => (p, e.effects.head)))
+    players.head.board.gold.amount should be(goldAmount)
+    players.head.board.sunCrystals.amount should be(0)
+    players(1).board.gold.amount should be(goldAmount)
+    players(1).board.sunCrystals.amount should be(0)
+    choiceCalls should be(2)
 
