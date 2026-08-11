397a6334316149ebce53514cbfda66f2818e9849
Galileo Foschini <galileo.foschini@studio.unibo.it>
2026-08-11 12:00:25 +0200

    Readded the ScalaDoc to Navigator after the changes

--- src/main/scala/controller/Navigator.scala
+++ src/main/scala/controller/Navigator.scala
@@ -5,10 +5,14 @@ import view.ViewComponents.{MainStage, ViewSceneFactory}
 trait ViewState
 
 trait Navigator[VS]:
+  /** Set the View content to the new ViewState
+   *
+   * @param viewState The instance to set as the new viewState
+   */
   def navigateTo(viewState: VS): Unit
 
 object Navigator:
-  private class NavigatorImpl[T, VS <: ViewState](mainStage: MainStage[T], viewSceneFactory: ViewSceneFactory[T,VS]) extends Navigator[VS]:
+  private class NavigatorImpl[T, VS <: ViewState](mainStage: MainStage[T], viewSceneFactory: ViewSceneFactory[T, VS]) extends Navigator[VS]:
 
     override def navigateTo(viewState: VS): Unit = mainStage.setContent(viewSceneFactory.createScene(viewState))
 
