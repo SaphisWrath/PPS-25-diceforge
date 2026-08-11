3e1d45063e2fcf6efd0d7ede6656b5b8cd91f1a4
gbgDalmont <lorenzo.dalmonte4@studio.unibo.it>
2026-08-11 07:57:32 +0200

    Updated ThrowEffectTest

--- src/test/scala/model/effects/ThrowEffectTest.scala
+++ src/test/scala/model/effects/ThrowEffectTest.scala
@@ -4,7 +4,7 @@ import mock.MockPlayer
 import model.Players.Color.{Black, Blue, Orange}
 import model.dice.Die
 import model.effects.Target.Self
-import model.effects.ThrowEffects.{ThrowSubtractEffect, ThrowTimesEffect}
+import model.effects.ThrowEffects.{CopyOtherThrowResults, PlainThrowEffect, ThrowSubtractEffect, ThrowTimesEffect}
 import model.resource.{Gold, PlayerBoard}
 import org.scalatest.flatspec.AnyFlatSpec
 
@@ -40,4 +40,34 @@ class ThrowEffectTest extends AnyFlatSpec:
     ThrowTimesEffect(throws).resolve(players)
     players.zipWithIndex.foreach((p, i) =>
       assert(p.board.gold.amount == expected(i))
+    )
+
+  "A PlainThrowEffect" should "let players throw their dice without getting the rewards from them" in:
+    val resEffect = ResourceEffect(Gold(3), Self)
+    val mockDie = Die(Seq(resEffect))
+    val players = newPlayers
+    newPlayers.foreach(p =>
+      p.dice = Seq(mockDie, mockDie)
+      p.board = PlayerBoard.emptyBoard
+    )
+    PlainThrowEffect().resolve(players)
+    players.foreach(p =>
+      assert(p.board.gold.amount == 0)
+      p.dice.foreach(d => assert(d.lastRolledEffect.isDefined))
+    )
+
+  "A CopyOtherThrowResults effect" should "let the player copy other player's results twice" in:
+    val resEffect = ResourceEffect(Gold(1), Self)
+    val mockDie = Die(Seq(resEffect))
+    val activePlayer = newPlayers.head
+    val otherPlayers = newPlayers.tail
+    activePlayer.board = PlayerBoard.emptyBoard
+    otherPlayers.foreach(_.dice = Seq(mockDie, mockDie))
+    PlainThrowEffect().resolve(otherPlayers)
+    CopyOtherThrowResults(Self).resolve(Seq(activePlayer))
+
+    assert(EffectManager().effectsToSolve.size == 2)
+    EffectManager().effectsToSolve.foreach((p, e) =>
+      assert(p.name == activePlayer.name)
+      assert(e.effects.contains(resEffect))
     )
